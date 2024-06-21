b3df1c11207609bfd319d8d15d4bf06f39583d93
st143sota <0tf7532y328090j@au.com>
2024-06-21 22:16:09 +0900

    Player.javaとMain.javaの修正内容をコミット

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello SEMonster");
+    System.out.println("SEMonster!");
+
+    // Playerクラスのオブジェクトを作成
+    Player player = new Player();
+
+    // モンスターをドロー
+    player.drawMonsters();
+
+    // デッキを表示
+    player.showDeck();
   }
 }
